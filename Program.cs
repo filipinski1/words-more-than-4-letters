821902b0cf948771c409075232d7151fd511bb00
Filipinski <140195559+filipinski1@users.noreply.github.com>
2023-10-23 14:52:44 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace words_more_than_4_letters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string continueInput;
+            do
+            {
+                Console.WriteLine("Please enter a string:");
+                string input = Console.ReadLine();
+                int count = CountWordsWithMoreThanFourLetters(input);
+                Console.WriteLine("Number of words with more than 4 letters: " + count);
+                do
+                {
+                    Console.WriteLine("Do you want to check another string ? (yes/no)");
+                    continueInput = Console.ReadLine().ToLower();
+                } while (continueInput != "yes" && continueInput != "no");
+            
+            } while (Console.ReadLine().ToLower() == "yes");
+            }
+            public static int CountWordsWithMoreThanFourLetters(string input)
+            {
+                // Split the input string into words
+                string[] words = input.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int count = 0;              
+                foreach (string word in words)
+                {
+                    if (word.Length > 4)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
